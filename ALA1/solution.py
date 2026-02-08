e8f07d741910b2af5015c7b01825f5f969fe9131
SHUBHAM8898 <SHUBHAMMAKWANA8898@GMAIL.COM>
2026-02-08 14:10:39 +0530

    Add program to find the maximum of three numbers

--- ALA1/solution.py
+++ ALA1/solution.py
@@ -1,1 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
 
+if a > b and a > c:
+     print("maximum number is:",a)
+elif b > a and b > c:
+     print("maximum number is:",b)
+else:
+     print("maximum number is:",c)
+  
